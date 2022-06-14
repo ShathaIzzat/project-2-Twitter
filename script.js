e6e967f66855ec45af1e4cb8d13b6dc2f941a0e1
ShathaIzzat <shathaizzat2@gmail.com>
2022-06-14 22:23:52 +0300

    project 2

---
+++ script.js
@@ -0,0 +1,25 @@
+let tweet_array =[];
+
+let tweetnumbwr=1;
+
+let tweetText  ;
+tweetText=document.getElementById("textarea").ariaValueText;
+
+let newUserInfo  ; 
+newUserInfo=document.getElementById("tweetUserInfo");
+
+var newTweet= {
+ n: newUserInfo,
+ t:tweetTextVal 
+};
+let tweetDiv = document.createElement("div");
+
+let button = document.getElementsByName("button");
+
+button.add()
+
+function pushInTweetArea () {
+tweet_array.push(newTweet);
+return tweetDiv.innerHTML = tweet_array.values;
+}
+
